a991fe791f2b45c644b60e7d7843f572950ff5f6
mohdarif7700 <47075030+mohdarif7700@users.noreply.github.com>
2019-03-22 17:35:00 +0530

    Update tqql.py

--- tqql.py
+++ tqql.py
@@ -1,26 +1,32 @@
 import discord
+# import sys
+# import os
+
 import asyncio
-
-
-
 
 bot_channel_id = discord.Object(id='557047819832393739')
 oot_channel_id_list = ["557047819832393739","525131707410677761","523359669280833536","513818250652680213"]
+
 
 sent_new_message = False
 answer_scores = {
     "1": 0,
     "2": 0,
-    "3": 0,  
-    
+    "3": 0,
+   
 }
 answer_scores_last = {
     "1": 0,
     "2": 0,
     "3": 0,
-   
-}
 
+}
+# def restart_program():
+#     """Restarts the current program.
+#     Note: this function does not return. Any cleanup action (like
+#     saving data) must be done before calling this function."""
+#     python = sys.executable
+#     os.execl(python, python, * sys.argv)
 apgscore = 150
 nomarkscore = 80
 markscore = 40
@@ -33,18 +39,21 @@ async def on_ready():
     
     print("Connected to discord.")
     
-    await bot.send_message(bot_channel_id, "```MATRICKS GAMING !``` Connected ")
+    await bot.send_message(bot_channel_id, "**READY ?```THE Q  Q LIVE TRIVIA```** ")
+   
+
+   
 
 @bot.event
 async def on_message(message):
     global sent_new_message
     global answer
+    global wrong
     global answer_scores
     global answer_scores_last
 
     if message.server == None:
         return
-    
     if message.content.lower() == "-q":
        if "554283064822333441" in [role.id for role in message.author.roles]:
            sent_new_message =False
@@ -53,18 +62,15 @@ async def on_message(message):
                 "2": 0,
                 "3": 0,
            }
-           wrong = ""
-           answer = "" 
-               
-       
-    
+           wrong = " "
+           answer = " " 
+           
 
 @selfbot.event
 async def on_ready():
     
     print("Connected to discord.")
     
-
 @selfbot.event
 async def on_message(message):
     global answer_scores
@@ -83,7 +89,7 @@ async def on_message(message):
         elif content == "2":
             answer_scores["2"] += nomarkscore
         elif content == "3":
-            answer_scores["3"] +=nomarkscore
+            answer_scores["3"] += nomarkscore
         
         elif content.startswith("1?") or content.startswith("1apg?"):
             answer_scores["1"] += markscore
@@ -91,7 +97,7 @@ async def on_message(message):
             answer_scores["2"] += markscore
         elif content.startswith("3?") or content.startswith("3apg?"):
             answer_scores["3"] += markscore
-        
+	
         elif content == "w1":
             answer_scores["1"] += apgscore
         elif content == "w2":
@@ -106,11 +112,11 @@ async def on_message(message):
         elif content == "3apg":
             answer_scores["3"] += apgscore
         
-        elif content in ["not1", "n1","N1"]:
+        elif content in ["not1", "n1"]:
             answer_scores["1"] -= nomarkscore
-        elif content in ["not2", "n2","N2"]:
+        elif content in ["not2", "n2"]:
             answer_scores["2"] -= nomarkscore
-        elif content in ["not3", "n3","N3"]:
+        elif content in ["not3", "n3"]:
             answer_scores["3"] -= nomarkscore
         
         elif content.startswith("not1?") or content.startswith("n1?"):
@@ -119,12 +125,13 @@ async def on_message(message):
             answer_scores["2"] -= markscore
         elif content.startswith("not3?") or content.startswith("n3?"):
             answer_scores["3"] -= markscore
-       
         elif content == "+r":
             answer_scores["1"] *= 0
             answer_scores["2"] *= 0
             answer_scores["3"] *= 0
             
+       
+       
         else:
             return
 
@@ -161,6 +168,7 @@ async def discord_send():
     while not bot.is_closed:
 	    
         if answer_scores != answer_scores_last:
+            
             if wrong and answer :
                 one_cross = ""
                 two_cross = ""
@@ -184,33 +192,35 @@ async def discord_send():
                 
                 if not sent_new_message:
                     
-                    embed=discord.Embed(title="THEQ QLIVE", description="", color=0xadd8e6)
+                    embed=discord.Embed(title="THE Q  Q LIVE", description="", color=0xadd8e6)
                     embed.add_field(name="A", value=f"{answer_scores['1']}{one_cross}{one_check}", inline=False)
                     embed.add_field(name="B", value=f"{answer_scores['2']}{two_cross}{two_check}", inline=False)
                     embed.add_field(name="C", value=f"{answer_scores['3']}{three_cross}{three_check}", inline=False)
                     
                     embed.set_footer(text=f"© MATRICKS GAMING | 7700", icon_url="")
-
+                   
                     answer_message = await send_embed(bot, embed)
                     sent_new_message = True
                 else:
                     
-                    embed=discord.Embed(title="THEQ QLIVE", description="", color=0xadd8e6)
+                    embed=discord.Embed(title="THE Q  Q LIVE", description="", color=0xadd8e6)
                     embed.add_field(name="A", value=f"{answer_scores['1']}{one_cross}{one_check}", inline=False)
                     embed.add_field(name="B", value=f"{answer_scores['2']}{two_cross}{two_check}", inline=False)
                     embed.add_field(name="C", value=f"{answer_scores['3']}{three_cross}{three_check}", inline=False)
                        
                     embed.set_footer(text=f"© MATRICKS GAMING | 7700", icon_url="")
-
                     x = await edit_embed(bot, answer_message, embed)
                     await bot.add_reaction(x,emoji="✅")
                     await bot.add_reaction(x,emoji="❌")
+                    
+		    
                 answer_scores_last = answer_scores.copy()
                 await asyncio.sleep(0)
                 continue
 
         answer_scores_last = answer_scores.copy()
         await asyncio.sleep(0)
+
 loop = asyncio.get_event_loop()
 loop.create_task(bot.start("NTU3OTEwODMxMjU0MzM5NTk3.D3Sh5w.3Sr9NO-7OOpywmqGiMtw0AVRoqM"))
 loop.create_task(selfbot.start("NDcxNzUzNjA2Nzk0NTc1ODky.D3DLjw.r9t-wvWsJq-IJ9Y7PvLU2DJ6_ZE", bot=False))
